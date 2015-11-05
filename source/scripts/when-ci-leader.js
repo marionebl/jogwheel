4da76a124c2924615c03d077ed32ca29984e2b57
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-11-05 18:08:56 +0100

    chore: optimize remote test runs

---
+++ source/scripts/when-ci-leader.js
@@ -0,0 +1,31 @@
+import chalk from 'chalk';
+
+
+function isLeader(jobNumber = '') {
+	const fragments = jobNumber.split('.');
+	return fragments[fragments.length - 1] === '1';
+}
+
+function main() {
+	const job = process.env.TRAVIS_JOB_NUMBER;
+
+	if (!job) {
+		console.log(`   ${chalk.green('✔')}   Skipping browser tests, "process.env.TRAVIS_JOB_NUMBER" is not defined.`);
+		throw new Error(1);
+	}
+
+	if (!isLeader(job)) {
+		console.log(`   ${chalk.green('✔')}   Skipping browser tests, job "${job}" is not the leader.`);
+		throw new Error(1);
+	}
+
+	if (isLeader(process.env.TRAVIS_JOB_NUMBER)) {
+		return;
+	}
+}
+
+try {
+	main();
+} catch (err) {
+	process.exit(1);
+}
