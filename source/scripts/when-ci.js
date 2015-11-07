b5517ec68839481ef11ee38823d377b5510fa107
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-11-07 08:02:54 +0100

    chore: sanitize travis setup

--- source/scripts/when-ci.js
+++ source/scripts/when-ci.js
@@ -1,17 +1,46 @@
 import chalk from 'chalk';
+import minimist from 'minimist';
 
-function main() {
+function isLeader(jobNumber = '') {
+	const fragments = jobNumber.split('.');
+	return fragments[fragments.length - 1] === '1';
+}
+
+function isTrusted() {
+	return process.env.TRAVIS_SECURE_ENV_VARS === 'true';
+}
+
+function main(options) {
 	const job = process.env.TRAVIS_JOB_NUMBER;
 
 	if (!job) {
+		console.log(`  ${chalk.yellow('⚠')}   Skipping, "$TRAVIS_JOB_NUMBER" is not defined.`);
 		throw new Error(1);
+	} else {
+		console.log(`  ${chalk.green('✔')}   Job number is "${job}".`);
 	}
 
-	return;
+	if (options.leader && !isLeader(job)) {
+		console.log(`  ${chalk.yellow('⚠')}   Skipping, job "${job}" is not the leader. ${chalk.gray('[--leader]')}`);
+		throw new Error(1);
+	} else if (options.leader) {
+		console.log(`  ${chalk.green('✔')}   Job "${job}" is the leader. ${chalk.gray('[--leader]')}`);
+	}
+
+	if (options.trusted && !isTrusted(job)) {
+		console.log(`  ${chalk.yellow('⚠')}   Skipping, job "${job}" has no secure env variables. ${chalk.grey('[--trusted]')}`);
+		throw new Error(1);
+	} else if (options.trusted) {
+		console.log(`  ${chalk.green('✔')}   Job "${job}" has secure env variables. ${chalk.grey('[--trusted]')}`);
+	}
+
+	if (isLeader(process.env.TRAVIS_JOB_NUMBER)) {
+		return;
+	}
 }
 
 try {
-	main();
+	main(minimist(process.argv.slice(2)));
 } catch (err) {
 	process.exit(1);
 }
