cf47b922e600733cb214285b78be62e5d6f71f75
Mario Nebl <hello@herebecode.com>
2015-11-12 22:36:19 +0100

    chore: tweaks on build system

--- source/scripts/pages-update.js
+++ source/scripts/pages-update.js
@@ -10,7 +10,7 @@ async function main(options) {
 	const start = Date.now();
 	const hash = shell.exec(`git rev-parse --short HEAD`, {silent: true}).output.split('\n')[0];
 
-	const branchName = `gh-pages-update-${hash}`;
+	const head = `gh-pages-update-${hash}`;
 	const remote = process.env.GH_TOKEN ?
 		`https://${process.env.GH_TOKEN}@github.com/${pkg.config.documentation.slug}.git` :
 		`origin`;
@@ -19,17 +19,15 @@ async function main(options) {
 	shell.exec(`git status --porcelain`, {silent: true});
 
 	shell.exec(`git commit -m "docs: ${hash} master → gh-pages"`, {silent: true});
-	console.log(`  ${chalk.gray('⧗')}   pushing to github.com/${pkg.config.documentation.slug}#${branchName}.`);
-	shell.exec(`git subtree --prefix=public/ push ${remote} ${branchName}`, {silent: true});
-	console.log(`  ${chalk.green('✔')}   pushed to github.com/${pkg.config.documentation.slug}#${branchName}.`);
-
+	console.log(`  ${chalk.gray('⧗')}   pushing to github.com/${pkg.config.documentation.slug}#${head}.`);
+	shell.exec(`git subtree --prefix=public/ push ${remote} ${head}`, {silent: true});
+	console.log(`  ${chalk.green('✔')}   pushed to github.com/${pkg.config.documentation.slug}#${head}.`);
 
 	const title = 'docs: ${hash} master → gh-pages';
 	const base = 'gh-pages';
-	const head = branchName;
 
 	if (process.env.CI && process.env.GH_TOKEN) {
-		console.log(`  ${chalk.gray('⧗')}   submitting pull request "${branchName} → gh-pages" via oauth`);
+		console.log(`  ${chalk.gray('⧗')}   submitting pull request "${head} → gh-pages" via oauth`);
 		const github = new Github({
 			auth: 'oauth',
 			token: process.env.GH_TOKEN
@@ -40,17 +38,18 @@ async function main(options) {
 
 		await denodeify(repository.createPullRequest)({
 			title,
-			base
+			base,
+			head
 		});
-		console.log(`  ${chalk.green('✔')}   submitted pull request "${branchName} → gh-pages" via oauth`);
+		console.log(`  ${chalk.green('✔')}   submitted pull request "${head} → gh-pages" via oauth`);
 	} else {
-		console.log(`  ${chalk.gray('⧗')}   submitting pull request "${branchName} → gh-pages" via hub`);
+		console.log(`  ${chalk.gray('⧗')}   submitting pull request "${head} → gh-pages" via hub`);
 		const pr = shell.exec(`hub pull-request -f -m "${title}" -b ${base} -h ${head}`, {silent: true});
 
 		if (pr.code === 0) {
-			console.log(`  ${chalk.green('✔')}   submitted pull request "${branchName} → gh-pages" via hub: ${pr.output.split('\n')[0]}`);
+			console.log(`  ${chalk.green('✔')}   submitted pull request "${head} → gh-pages" via hub: ${pr.output.split('\n')[0]}`);
 		} else {
-			console.log(`  ${chalk.red('✖')}   failed to submit pull request "${branchName} → gh-pages" via hub`);
+			console.log(`  ${chalk.red('✖')}   failed to submit pull request "${head} → gh-pages" via hub`);
 			console.log(pr.output);
 		}
 	}
