3d5e4254709f1a59a43bd3f59bdb03c8a425b3ff
Mario Nebl <hello@herebecode.com>
2015-11-15 23:45:29 +0100

    fix: handle timing-function, iteration-count properly - #76

---
+++ source/test/unit/fixtures/filled-animation.js
@@ -0,0 +1,8 @@
+export default {
+	animationName: 'filled-animation',
+	animationPlayState: 'running',
+	animationTimingFunction: 'linear',
+	animationDuration: '1s',
+	animationFillMode: 'both',
+	animationIterationCount: '10'
+};
---
+++ source/test/unit/get-animation-properties.js
@@ -0,0 +1,77 @@
+import tape from 'tape';
+import getAnimationProperties from '../../library/get-animation-properties.js';
+
+import elementStub from './stubs/element.js';
+import windowStub from './stubs/window.js';
+import documentStub from './stubs/document.js';
+
+import pausedAnimation from './fixtures/paused-animation';
+import filledAnimation from './fixtures/filled-animation';
+
+tape('get-animation-properties', t => {
+	t.throws(() => {
+		getAnimationProperties();
+	}, 'should throw when called without arguments');
+
+	t.doesNotThrow(() => {
+		getAnimationProperties(elementStub, windowStub, documentStub);
+	}, 'should not throw when called with arguments');
+
+	const pausedElement = {
+		...elementStub,
+		style: {
+			...elementStub.style,
+			...pausedAnimation
+		}
+	};
+
+	const pausedProperties = getAnimationProperties(pausedElement, windowStub, documentStub);
+
+	t.deepEqual(
+		Object.keys(pausedProperties),
+		[
+			'name',
+			'duration',
+			'iterationCount',
+			'timingFunction',
+			'fillMode',
+			'playState'
+		],
+		'should return an object with the expected property values');
+
+	t.deepEqual(
+		pausedProperties,
+		{
+			name: 'default-animation',
+			duration: '300ms',
+			iterationCount: undefined,
+			timingFunction: 'linear',
+			fillMode: undefined,
+			playState: 'paused'
+		},
+		'should return an object with the expected property values');
+
+	const filledElement = {
+		...elementStub,
+		style: {
+			...elementStub.style,
+			...filledAnimation
+		}
+	};
+
+	const filledProperties = getAnimationProperties(filledElement, windowStub, documentStub);
+
+	t.deepEqual(
+		filledProperties,
+		{
+			name: 'filled-animation',
+			duration: '1s',
+			iterationCount: '10',
+			timingFunction: 'linear',
+			fillMode: 'both',
+			playState: 'running'
+		},
+		'should return an object with the expected property values');
+
+	t.end();
+});
